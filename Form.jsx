21b38e0bac299c22d6d390d96788c80ceb79e988
moreiraA <138811815+francinyarielly@users.noreply.github.com>
2024-04-19 18:22:29 -0300

    Add files via upload

---
+++ Form.jsx
@@ -0,0 +1,31 @@
+import "./Form.css"
+
+
+const Form = () => {
+
+    window.addEventListener
+
+    return(
+<>
+        <h1>Learn to code by watching others</h1>
+            
+
+        
+        <div className="Forms">
+            <form action="">
+                <input className="caixa" type="text" value="First Name" />
+                
+                <input className="caixa" type="text" value="Last Name"/>
+                
+                <input className="caixa" type="email" value="Email Address"/>
+                
+                <input className="caixa" type="text" value="Password"/> <br></br>
+                
+                <input className="Button" type="submit" value="Claim your free trial"/>
+                 <p>By clicking the button, you are agreeing to our Tearms and Services </p>
+            </form>
+        </div>
+        </>
+    )
+}
+export default Form;
